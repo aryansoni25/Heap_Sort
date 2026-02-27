be786c4c805c8373124ad59a7582ab6d6322538b
aryansoni25 <ashoksoni9425@gmail.com>
2026-02-27 09:38:27 +0530

    Minor Changes

--- src/main/java/org/studyeasy/HeapSort.java
+++ src/main/java/org/studyeasy/HeapSort.java
@@ -14,7 +14,6 @@ public class HeapSort {
             arr[i]= bh.extractHeadOfBP("Min");
         }
     }
-
     public static void printArray(int[] arr){
         for(int i=0;i< arr.length;i++){
             System.out.print(arr[i]+" ");
